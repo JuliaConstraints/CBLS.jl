6f8b6d8b85af2d015a8df78516192bed5fbf6b6c
Documenter.jl <documenter@juliadocs.github.io>
2021-03-21 16:04:41 +0000

    build based on 4a32718

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CBLS","category":"page"},{"location":"#CBLS","page":"Home","title":"CBLS","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CBLS.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CBLS]","category":"page"}]
+}
