8d654f8a332ef85c6c39035f554c4f0f4ed4c73f
azzaare <jf@baffier.fr>
2021-05-26 11:42:21 +0900

    Add MOI.NumberOfThreads()

--- src/attributes.jl
+++ src/attributes.jl
@@ -2,15 +2,19 @@ struct PrintLevel <: MOI.AbstractOptimizerAttribute end
 
 MOI.supports(::Optimizer, ::MOI.RawParameter) = true
 MOI.supports(::Optimizer, ::MOI.TimeLimitSec) = true
+MOI.supports(::Optimizer, ::MOI.NumberOfThreads) = true
 
 """
-    MOI.set(model::Optimizer, ::MOI.RawParameter, value)
+    MOI.set(model::Optimizer, ::MOI.TimeLimitSec, value::Union{Nothing,Float64})
 Set the time limit
 """
 function MOI.set(model::Optimizer, ::MOI.TimeLimitSec, value::Union{Nothing,Float64})
-    set_option!(model, "time_limit", value === nothing ? Inf : value)
+    set_option!(model, "time_limit", isnothing(value) ? Inf : value)
 end
-MOI.get(model::Optimizer, ::MOI.TimeLimitSec) = get_option(model, "time_limit")
+function MOI.get(model::Optimizer, ::MOI.TimeLimitSec)
+    tl = get_option(model, "time_limit")
+    return isinf(tl) ? nothing : tl
+end
 
 """
     MOI.set(model::Optimizer, p::MOI.RawParameter, value)
@@ -18,3 +22,12 @@ Set a RawParameter to `value`
 """
 MOI.set(model::Optimizer, p::MOI.RawParameter, value) = set_option!(model, p.name, value)
 MOI.get(model::Optimizer, p::MOI.RawParameter) = get_option(model, p.name)
+
+
+function MOI.set(model::Optimizer, ::MOI.NumberOfThreads, value)
+    set_option!(model, "threads", isnothing(value) ? typemax(0) : value)
+end
+function MOI.get(model::Optimizer, ::MOI.NumberOfThreads)
+    nt = get_option(model, "threads")
+    return nt == typemax(0) ? nothing : nt
+end
